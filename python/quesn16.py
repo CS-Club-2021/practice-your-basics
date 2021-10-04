cc2fe0823777806a0d83dc251b16ee3d45a6ce93
Hitesh1309 <81859555+Hitesh1309@users.noreply.github.com>
2021-10-05 04:47:33 +0530

    Solved Question 16 using Python

---
+++ python/quesn16.py
@@ -0,0 +1,7 @@
+# Sovled Question 16.
+# Concatenating two strings and printing them.
+
+s1 = input('Please enter the first string:\n')
+s2 = input('Please enter the second string:\n')
+
+print('Concatenated String :', s1 + s2)
