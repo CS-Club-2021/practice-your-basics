8d6b118e39d9bcac706bdbc121fcfea03c7e0dec
Apara Biswas <85570705+algo-geek@users.noreply.github.com>
2021-10-11 11:10:54 +0530

    Update quesn17.cpp

--- cpp/quesn17.cpp
+++ cpp/quesn17.cpp
@@ -1,6 +1,6 @@
 
-//17. Define a function that can accept two strings as input and print the string with maximum length in console. If two strings have the same length, then the function should print all strings line by line.
-
+//17. Define a function that can accept two strings as input and print the string with maximum length in console. If two strings have the same length, then the function
+//    should print all strings line by line.
 
 #include <bits/stdc++.h>
 using namespace std;
