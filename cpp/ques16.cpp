ba8d0f23e040bef0b1bb0ea47efe0c784bf65d92
kriti-arora <92243363+kriti-arora@users.noreply.github.com>
2021-10-14 23:40:07 +0530

    Create ques16.cpp
    Concatenation of two strings

---
+++ cpp/ques16.cpp
@@ -0,0 +1,18 @@
+// Ques 16 by Kriti Arora
+// Student-- UIET KURUKSHETRA, HARYANA
+// Institute Roll No.-- 252002007
+
+//Program starts now
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+	string s1,s2; //declaration of two strings
+	cin>>s1>>s2; //taking input of two strings
+	
+	cout<<s1+s2<<endl; // output 
+	
+	return 0;
+}
